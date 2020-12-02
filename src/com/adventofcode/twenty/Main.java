c5d75247e1eea655ae277981284b7d87f448d919
an4 <anadumitras12@gmail.com>
2020-12-02 22:40:53 +0100

    Day 2

--- src/com/adventofcode/twenty/Main.java
+++
@@ -1,8 +0,0 @@
-package com.adventofcode.twenty;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ src/day02/Solution.java
@@ -0,0 +1,78 @@
+package day02;
+
+import java.io.*;
+import java.net.URL;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+
+    public static void main(String[] args) throws IOException {
+        List<String> input = processInput();
+        countValidPasswordsWithCount(input);
+        countValidPasswordsWithPosition(input);
+    }
+
+    private static void countValidPasswordsWithCount(List<String> list) {
+        System.out.println(list.stream().filter(Solution::isValidPasswordWithCount).count());
+    }
+
+    private static void countValidPasswordsWithPosition(List<String> list) {
+        System.out.println(list.stream().filter(Solution::isValidPasswordWithPosition).count());
+    }
+
+    private static boolean isValidPasswordWithCount(String input) {
+        String[] inputItems = input.split(":");
+        assert(inputItems.length == 2);
+        String password = inputItems[1].trim();
+        String policy = inputItems[0];
+        String[] policyItems = policy.split(" ");
+        assert(policyItems.length == 2);
+        String count = policyItems[0].trim();
+        String[] bounds = count.split("-");
+        assert(bounds.length == 2);
+        int lowerBound = Integer.parseInt(bounds[0]);
+        int upperBound = Integer.parseInt(bounds[1]);
+        String character = policyItems[1].trim();
+        assert (character.length() == 1);
+        char letter = character.charAt(0);
+
+        int letterCount = 0;
+        for (int i=0; i<password.length(); i++) {
+            if (password.charAt(i) == letter) {
+                letterCount++;
+            }
+        }
+        return letterCount >= lowerBound && letterCount <= upperBound;
+    }
+
+    private static boolean isValidPasswordWithPosition(String input) {
+        String[] inputItems = input.split(":");
+        assert(inputItems.length == 2);
+        String password = inputItems[1].trim();
+        String policy = inputItems[0];
+        String[] policyItems = policy.split(" ");
+        assert(policyItems.length == 2);
+        String count = policyItems[0].trim();
+        String[] bounds = count.split("-");
+        assert(bounds.length == 2);
+        int position1 = Integer.parseInt(bounds[0]) - 1;
+        int position2 = Integer.parseInt(bounds[1]) - 1;
+        String character = policyItems[1].trim();
+        assert (character.length() == 1);
+        char letter = character.charAt(0);
+
+        boolean isLetterInFirstPosition = password.charAt(position1) == letter;
+        boolean isLetterInSecondPosition = password.charAt(position2) == letter;
+        return isLetterInFirstPosition != isLetterInSecondPosition;
+    }
+
+    private static List<String> processInput() throws IOException {
+        URL path = Solution.class.getResource("input.txt");
+        File f = new File(path.getFile());
+        try (BufferedReader br = new BufferedReader(new FileReader(f))) {
+            return br.lines().collect(Collectors.toList());
+        }
+    }
+}
